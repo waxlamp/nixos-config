b5e44c42caa81b63d3643718eafba5c52d56d672
Roni Choudhury <roni.choudhury@kitware.com>
2019-05-19 18:20:52 -0400

    Capture current setup

---
+++ machines/kahless/configuration.nix
@@ -0,0 +1,238 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  fileSystems."/home" = {
+    device = "/dev/sda2";
+    fsType = "ext4";
+  };
+
+  #fileSystems."/mnt/kitwarenas5" = {
+    #device = "//kitwarenas5/Share";
+    #fsType = "cifs";
+    #options =
+      #let
+        ## this line prevents hanging on network split
+        #automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+      #in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+  #};
+
+  #boot.kernelPackages = pkgs.linuxPackages_3_18;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  time.timeZone = "America/New_York";
+
+  # Use the gummiboot efi boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.timeout = 5;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Select internationalisation properties.
+  i18n = {
+    #consoleFont = "lat9w-16";
+    consoleFont = "LatArCyrHeb-sun32";
+    consoleKeyMap = "us";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  # Set nix path.
+  #nix.nixPath = [
+    #"nixpkgs=/home/roni/.nix-defexpr/channels/nixos/nixpkgs"
+    #"nixos-config=/etc/nixos/configuration.nix"
+  #];
+
+  # Copy system config to the store.
+  system.copySystemConfiguration = true;
+
+  # List packages installed in system profile. To search by name, run:
+  # -env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    zsh
+    #mesa
+    pmutils
+    #(wine.override { wineBuild = "wineWow"; })
+    networkmanagerapplet
+    docker_compose
+
+    # Haskell packages for XMonad
+    xmonad-with-packages
+  ];
+
+  environment.shells = [
+    "/run/current-system/sw/bin/zsh"
+  ];
+
+  networking = {
+    hostName = "kahless";
+    useDHCP = false;
+    networkmanager = {
+        enable = true;
+    };
+    wireless.enable = false;
+  };
+
+  # List services that you want to enable:
+  services = {
+    # Locate service.
+    locate.enable = true;
+
+    # Enable the OpenSSH daemon.
+    openssh.enable = true;
+
+    # Keybase service
+    keybase.enable = true;
+
+    # Enable CUPS to print documents.
+    printing.enable = true;
+    printing.drivers = [
+      #pkgs.brlaser
+      #pkgs.hll2390dw-cups
+      (pkgs.callPackage ../../printers/hll2395dw-cups.nix {})
+    ];
+
+    # Fix to enable trayer/nm-applet to start properly
+    # (https://github.com/NixOS/nixpkgs/issues/16327#issuecomment-227218371).
+    gnome3.at-spi2-core.enable = true;
+
+    # Enable the X11 windowing system.
+    xserver = {
+      enable = true;
+      layout = "us";
+      videoDrivers = ["intel"];
+
+      windowManager = {
+          xmonad = {
+              enable = true;
+              enableContribAndExtras = true;
+          };
+
+          default = "xmonad";
+      };
+
+      desktopManager.xterm.enable = false;
+
+      displayManager = {
+        sessionCommands = ''
+          sh ~/.xinitrc
+        '';
+
+        # Uncomment to restore default SLiM theme.
+        #slim.theme = null;
+      };
+
+      synaptics = {
+          enable = false;
+          palmDetect = true;
+          tapButtons = true;
+          twoFingerScroll = true;
+      };
+
+      libinput = {
+        enable = true;
+        clickMethod = "clickfinger";
+        additionalOptions = ''
+          Option "TappingButtonMap" "lmr"
+        '';
+      };
+    };
+
+    # Power management.
+    logind = {
+      lidSwitch = "ignore";
+      extraConfig = ''
+          HandlePowerKey=ignore
+      '';
+    };
+    acpid = {
+      enable = true;
+      lidEventCommands =
+        ''
+          export PATH=$PATH:/run/current-system/sw/bin
+
+          lid_state=$(cat /proc/acpi/button/lid/LID0/state | awk '{print $NF}')
+          if [ $lid_state = "closed" ]; then
+              systemctl suspend
+          fi
+        '';
+
+      powerEventCommands =
+        ''
+          systemctl suspend
+        '';
+    };
+
+    # Kubernetes.
+    #kubernetes = {
+      #roles = ["master" "node"];
+    #};
+  };
+
+  virtualisation = {
+    docker.enable = true;
+    virtualbox = {
+      guest = {
+        enable = false;
+      };
+
+      host = {
+        enable = false;
+        enableHardening = true;
+        addNetworkInterface = true;
+      };
+    };
+  };
+
+  # PulseAudio
+  hardware.pulseaudio.enable = true;
+
+  # Bluetooth
+  hardware.bluetooth.enable = true;
+
+  # 32-bit DRI support.
+  hardware.opengl.driSupport32Bit = true;
+
+  nixpkgs.config.allowUnfree = true;
+
+  #security.grsecurity.enable = false;
+
+  # Enable the KDE Desktop Environment.
+  # services.xserver.displayManager.kdm.enable = true;
+  # services.xserver.desktopManager.kde4.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # users.extraUsers.guest = {
+  #   name = "guest";
+  #   group = "users";
+  #   uid = 1000;
+  #   createHome = true;
+  #   home = "/home/guest";
+  #   shell = "/run/current-system/sw/bin/bash";
+  # };
+
+  users.extraUsers = {
+    roni = {
+      name = "roni";
+      group = "users";
+      extraGroups = [
+        "wheel"
+        "vboxusers"
+        "docker"
+        "networkmanager"
+      ];
+      uid = 1000;
+      home = "/home/roni";
+      shell = "/run/current-system/sw/bin/zsh";
+      packages = with pkgs; [
+        hello
+      ];
+    };
+  };
+}
---
+++ printers/hll2395dw-cups.nix
@@ -0,0 +1,71 @@
+{ stdenv, fetchurl, makeWrapper
+, cups
+, dpkg
+, a2ps, ghostscript, gnugrep, gnused, coreutils, file, perl, which
+}:
+
+stdenv.mkDerivation rec {
+  name = "hll2395dw-cups-${version}";
+  version = "4.0.0-1";
+
+  src = fetchurl {
+    url = "https://download.brother.com/welcome/dlf103564/hll2395dwpdrv-4.0.0-1.i386.deb";
+    sha256 = "18zy039liqrh0wykays0b4np9mjfy233s8il0d9njbiwl7nnz9p2";
+  };
+
+  nativeBuildInputs = [ makeWrapper ];
+  buildInputs = [ cups ghostscript dpkg a2ps ];
+
+  unpackPhase = ":";
+
+  installPhase = ''
+    dpkg-deb -x $src $out
+
+    substituteInPlace $out/opt/brother/Printers/HLL2395DW/lpd/lpdfilter \
+      --replace /opt "$out/opt" \
+      --replace /usr/bin/perl ${perl}/bin/perl \
+      --replace "BR_PRT_PATH =~" "BR_PRT_PATH = \"$out\"; #" \
+      --replace "PRINTER =~" "PRINTER = \"HLL2395DW\"; #"
+
+    # FIXME : Allow i686 and armv7l variations to be setup instead.
+    _PLAT=x86_64
+    patchelf --set-interpreter $(cat $NIX_CC/nix-support/dynamic-linker) \
+      $out/opt/brother/Printers/HLL2395DW/lpd/$_PLAT/brprintconflsr3
+    patchelf --set-interpreter $(cat $NIX_CC/nix-support/dynamic-linker) \
+      $out/opt/brother/Printers/HLL2395DW/lpd/$_PLAT/rawtobr3
+    ln -s $out/opt/brother/Printers/HLL2395DW/lpd/$_PLAT/brprintconflsr3 $out/opt/brother/Printers/HLL2395DW/lpd/brprintconflsr3
+    ln -s $out/opt/brother/Printers/HLL2395DW/lpd/$_PLAT/rawtobr3 $out/opt/brother/Printers/HLL2395DW/lpd/rawtobr3
+
+    for f in \
+      $out/opt/brother/Printers/HLL2395DW/cupswrapper/lpdwrapper \
+      $out/opt/brother/Printers/HLL2395DW/cupswrapper/paperconfigml2 \
+    ; do
+      #substituteInPlace $f \
+      wrapProgram $f \
+        --prefix PATH : ${stdenv.lib.makeBinPath [
+          coreutils ghostscript gnugrep gnused
+        ]}
+    done
+
+    # Hack suggested by samueldr.
+    sed -i"" "s;A4;Letter;g" $out/opt/brother/Printers/HLL2395DW/inf/brHLL2395DWrc
+
+    mkdir -p $out/lib/cups/filter/
+    ln -s $out/opt/brother/Printers/HLL2395DW/lpd/lpdfilter $out/lib/cups/filter/brother_lpdwrapper_HLL2395DW
+
+    mkdir -p $out/share/cups/model
+    ln -s $out/opt/brother/Printers/HLL2395DW/cupswrapper/brother-HLL2395DW-cups-en.ppd $out/share/cups/model/
+
+    wrapProgram $out/opt/brother/Printers/HLL2395DW/lpd/lpdfilter \
+      --prefix PATH ":" ${ stdenv.lib.makeBinPath [ ghostscript a2ps file gnused gnugrep coreutils which ] }
+    '';
+
+  meta = with stdenv.lib; {
+    homepage = "https://www.brother.com/";
+    description = "Brother HL-L2395DW combined print driver";
+    license = licenses.unfree;
+    platforms = [
+      "x86_64-linux"
+    ];
+  };
+}
