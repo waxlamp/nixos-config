fecabc6110be90a1610eb6562d74ad316e09c164
Roni Choudhury <roni.choudhury@kitware.com>
2019-10-20 20:05:48 -0400

    Add working derivation for frobtads

---
+++ nixpkgs/overlays/frobtads.nix
@@ -0,0 +1,16 @@
+self: super:
+
+{
+  frobtads = self.stdenv.mkDerivation {
+    name = "frobtads-1.2.3";
+    src = self.fetchurl {
+      url = https://www.tads.org/frobtads/frobtads-1.2.3.tar.gz;
+      sha256 = "88c6a987813d4be1420a1c697e99ecef4fa9dd9bc922be4acf5a3054967ee788";
+    };
+    buildInputs = with self.pkgs; [
+      ncurses.dev
+      curl.dev
+    ];
+    patches = [ ./fix-pointer-type.patch ];
+  };
+}
