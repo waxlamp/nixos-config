e8bdf19fd43cbe6fd8942dbc4ca876517a62bdd4
Roni Choudhury <roni.choudhury@kitware.com>
2021-03-23 14:08:37 -0400

    Add home.nix file

---
+++ nixpkgs/home.nix
@@ -0,0 +1,75 @@
+{ config, pkgs, ... }:
+
+{
+  # Let Home Manager install and manage itself.
+  programs = {
+    home-manager.enable = true;
+
+    command-not-found.enable = false;
+  };
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "roni";
+  home.homeDirectory = "/home/roni";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "21.03";
+
+  home.packages = with pkgs; [
+    acpi
+    bat
+    fzf
+    htop
+    jetbrains-mono
+    jq
+    kbfs
+    keybase-gui
+    lsof
+    magic-wormhole
+    bitwarden-cli
+    brave
+    colordiff
+    diff-so-fancy
+    evince
+    fd
+    file
+    flameshot
+    gitFull
+    killall
+    mpv
+    ncdu
+    neovim
+    nodejs
+    pavucontrol
+    pipenv
+    python3
+    python38Packages.poetry
+    rhythmbox
+    signal-desktop
+    silver-searcher
+    slack
+    spotify
+    sweethome3d.application
+    tmux
+    tmuxinator
+    trayer
+    unclutter
+    unzip
+    xclip
+    xlockmore
+    xorg.xbacklight
+    xorg.xmodmap
+    xss-lock
+    yadm
+    yarn
+    zoom-us
+  ];
+}
