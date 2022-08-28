0c39b675a1bb58687b2c67ee1d0e6c7cc0ef73a8
Roni Choudhury <roni.choudhury@kitware.com>
2022-08-28 18:58:28 -0400

    Reactivate Kitware NAS mount

--- machines/kahless/configuration.nix
+++ machines/kahless/configuration.nix
@@ -15,15 +15,16 @@
     fsType = "ext4";
   };
 
-  #fileSystems."/mnt/kitwarenas5" = {
-    #device = "//kitwarenas5/Share";
-    #fsType = "cifs";
-    #options =
-      #let
-        ## this line prevents hanging on network split
-        #automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-      #in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
-  #};
+  fileSystems."/mnt/kitwarenas2" = {
+    device = "//kitwarenas2/Share";
+    fsType = "cifs";
+    options =
+      let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,vers=3.0";
+      in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+      #in ["credentials=/etc/nixos/smb-secrets"];
+  };
 
   #boot.kernelPackages = pkgs.linuxPackages_3_18;
   boot.kernelPackages = pkgs.linuxPackages_latest;
