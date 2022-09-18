0c34ad37e7faf9d0c799dc4764eaf5036a326dc2
Roni Choudhury <roni.choudhury@kitware.com>
2022-09-18 16:11:32 -0400

    Remove low-information comment

--- machines/kahless/packages.nix
+++ machines/kahless/packages.nix
@@ -1,8 +1,6 @@
 { nixpkgs, ... }:
 
 {
-  # List packages installed in system profile. To search by name, run:
-  # -env -qaP | grep wget
   environment.systemPackages = with nixpkgs; [
     pmutils
     networkmanagerapplet
