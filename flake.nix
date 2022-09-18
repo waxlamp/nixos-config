37792ecf7d0a219ab5c6dffbf4ac4764fb81335b
Roni Choudhury <roni.choudhury@kitware.com>
2022-09-17 22:38:08 -0400

    Refactor potentially reusable values

--- flake.nix
+++ flake.nix
@@ -15,10 +15,36 @@
   outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, elgato }:
   let
     system = "x86_64-linux";
+
+    # Module to set up the flakes registry with the same package sets as the
+    # system configuration uses.
+    flake-registry-module = { ... }: {
+      nix.registry = {
+        nixpkgs.flake = nixpkgs;
+        nixpkgs-unstable.flake = nixpkgs-unstable;
+        elgato.flake = elgato;
+      };
+    };
+
+    # Config for home-manager module.
+    home-manager-config = {
+      home-manager = {
+        useGlobalPkgs = true;
+        useUserPackages = true;
+        users.roni = import ./home.nix;
+
+        extraSpecialArgs = specialArgs;
+      };
+    };
+
+    # Convert a flake into a package set (allow unfree packages by default).
     unflake = flake: import flake {
       inherit system;
       config.allowUnfree = true;
     };
+
+    # Arrange to pass the instantiated package sets into every module (via the
+    # `specialArgs` value).
     nixpkgs' = unflake nixpkgs;
     specialArgs = {
       nixpkgs = nixpkgs';
@@ -33,25 +59,8 @@
 
       modules = [
         ./machines/kahless/configuration.nix
-
-        home-manager.nixosModules.home-manager {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users.roni = import ./home.nix;
-
-            extraSpecialArgs = specialArgs;
-          };
-        }
-
-        # Set up nix registry with nixpkgs flakes.
-        ({ ... }: {
-          nix.registry = {
-            nixpkgs.flake = nixpkgs;
-            nixpkgs-unstable.flake = nixpkgs-unstable;
-            elgato.flake = elgato;
-          };
-        })
+        home-manager.nixosModules.home-manager home-manager-config
+        flake-registry-module
       ];
     };
   };
