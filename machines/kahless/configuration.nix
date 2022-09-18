0af86822277d6202a86cec3313f59aa31794b75e
Roni Choudhury <roni.choudhury@kitware.com>
2022-09-18 15:44:01 -0400

    Move kitwarenas2 mount into own module

--- machines/kahless/configuration.nix
+++ machines/kahless/configuration.nix
@@ -10,17 +10,8 @@
       ./hardware-configuration.nix
       ./system.nix
       ./console.nix
+      ./mounts.nix
     ];
-
-  fileSystems."/mnt/kitwarenas2" = {
-    device = "//kitwarenas2/Share";
-    fsType = "cifs";
-    options =
-      let
-        # this line prevents hanging on network split
-        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,vers=3.0";
-      in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
-  };
 
   # Install Nix 2.11 from unstable.
   nix.package = nixpkgs-unstable.nix;
