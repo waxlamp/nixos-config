716d996b4b747ba9a965dbb3f21b8070e8cb387a
Roni Choudhury <roni.choudhury@kitware.com>
2022-09-21 22:25:19 -0400

    Remove unnecessary nixpkgs.config line
    The setup for `nixpkgs` before it is passed into home.nix already sets
    `allowUnfree` to `true`.

--- home.nix
+++ home.nix
@@ -5,8 +5,6 @@ let
   nixpkgs-unstable = specialArgs.nixpkgs-unstable;
   elgato = specialArgs.elgato;
 in {
-  nixpkgs.config.allowUnfree = true;
-
   # Let Home Manager install and manage itself.
   programs = {
     home-manager.enable = true;
